aca721ec6d504b4225863a4568123a495f17ed09
Filip Gokstorp <filip@gokstorp.se>
2018-07-09 16:50:20 +0100

    Adds some missing symbols to raise layer.

--- keyboards/lets_split/keymaps/fkag2/keymap.c
+++ keyboards/lets_split/keymaps/fkag2/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT, \
    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSPC, \
    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-   ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+   ADJUST,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,  KC_SPC,  KC_SPC,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Base layer (Colemak Mod-DH)
@@ -127,19 +127,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
    *            ┌─────┬─────┬─────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬─────┬─────┬─────┐
-   *            │     │ F1  │ F2  │ F3  │ F4  │  {  │   │  }  │  &  │  *  │     │     │     │
+   *            │     │ F1  │ F2  │ F3  │ F4  │  {  │   │  }  │  &  │  *  │  \  │  |  │  ~  │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │ F5  │ F6  │ F7  │ F8  │  (  │   │  )  │  $  │  %  │  ^  │     │     │
+   *            │     │ F5  │ F6  │ F7  │ F8  │  (  │   │  )  │  $  │  %  │  ^  │  `  │     │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │ F9  │ F10 │ F11 │ F12 │  [  │   │  ]  │  !  │  @  │  #  │     │     │
+   *            │     │ F9  │ F10 │ F11 │ F12 │  [  │   │  ]  │  !  │  @  │  #  │  _  │     │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
    *            │     │     │     │     │     │     │   │     │     │     │     │     │     │
    *            └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
    */
 [_RAISE] = LAYOUT_ortho_4x12( \
-  _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , S(KC_LBRC), S(KC_RBRC), S(KC_7), S(KC_8), ___x___, ___x___, ___x___, \
-  _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , S(KC_9)   ,    S(KC_0), S(KC_4), S(KC_5), S(KC_6), ___x___, _______, \
-  _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, KC_LBRC   ,    KC_RBRC, S(KC_1), S(KC_2), S(KC_3), ___x___, _______, \
+  _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , S(KC_LBRC), S(KC_RBRC), S(KC_7), S(KC_8), KC_BSLS, KC_PIPE, KC_TILD, \
+  _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , S(KC_9)   ,    S(KC_0), S(KC_4), S(KC_5), S(KC_6), KC_GRV , _______, \
+  _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, KC_LBRC   ,    KC_RBRC, S(KC_1), S(KC_2), S(KC_3), KC_UNDS, _______, \
   _______, _______, _______, _______, _______, _______   ,    _______, _______, _______, _______, _______, _______  \
 ),
 
