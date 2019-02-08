d328e2346da47e8842d82512657754fd1f55a392
Filip Gokstorp <filip@gokstorp.se>
2019-02-08 14:19:44 +0000

    Re-designs navigation keys and adds tap dance keys.

--- keyboards/lets_split/keymaps/fkag2/config.h
+++ keyboards/lets_split/keymaps/fkag2/config.h
@@ -34,4 +34,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_RIGHT
 // #define EE_HANDS
 
+/* Tap dance timing */
+#define TAPPING_TERM 180
+
 #endif
--- keyboards/lets_split/keymaps/fkag2/keymap.c
+++ keyboards/lets_split/keymaps/fkag2/keymap.c
@@ -15,27 +15,42 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 1
 #define _LOWER 4
 #define _RAISE 5
-#define _NAV 6
-#define _ADJUST 16
+#define _CENTER 16
 
 enum custom_keycodes {
   NORMAN = SAFE_RANGE,
   QWERTY,
   LOWER,
   RAISE,
-  NAV,
-  ADJUST,
+  CENTER,
+};
+
+// Tap dance definitions
+enum tap_dances {
+  TD_L = 0,  // Left parens, brace
+  TD_R,  // Right parens, brace
+  TD_E  // 0 or e
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_L]  = ACTION_TAP_DANCE_DOUBLE(KC_LPRN, KC_LCBR),
+  [TD_R]  = ACTION_TAP_DANCE_DOUBLE(KC_RPRN, KC_RCBR),
+  [TD_E]  = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_E)
 };
 
 // Mod taps
-#define SFT_ENT SFT_T(KC_ENT)  // Shift / ⇧ when held, Enter when tapped
-#define SFT_TAB SFT_T(KC_TAB)  // Shift / ⇧ when held, Tab when tapped
-#define SFT_ESC SFT_T(KC_ESC)  // Shift / ⇧ when held, Escape when tapped
-#define CTL_ESC CTL_T(KC_ESC)  // Control / ⌃ when held, Escape when tapped
-#define CTL_TAB CTL_T(KC_TAB)  // Control / ⇧ when held, Tab when tapped
+#define SFT_ENT SFT_T(KC_ENT)  // Shift when held, Enter when tapped
+#define SFT_TAB SFT_T(KC_TAB)  // Shift when held, Tab when tapped
+#define SFT_ESC SFT_T(KC_ESC)  // Shift when held, Escape when tapped
+#define CTL_ESC CTL_T(KC_ESC)  // Control when held, Escape when tapped
+#define CTL_TAB CTL_T(KC_TAB)  // Control when held, Tab when tapped
 
-// Nav layer trigger
-#define NAV_T(kc) LT(_NAV, kc)  // Nav layer when held, kc when tapped
+// Tap braces / brackets / parens
+#define HYPER_L ALL_T(KC_LBRC)  // Hyper when held, { when tapped
+#define HYPER_R ALL_T(KC_RBRC)  // Hyper when held, } when tapped
+#define TAP_L TD(TD_L)  // (, {
+#define TAP_R TD(TD_R)  // ), }
+#define TAP_0E TD(TD_E) // 0, e
 
 // Mac desktop movement keys
 #define DSK_L LCTL(KC_LEFT)
@@ -62,7 +77,7 @@ enum custom_keycodes {
 #define _________________NORMAN_L3_________________ KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
 
 #define _________________NORMAN_R1_________________ KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN
-#define _________________NORMAN_R2_________________ KC_Y,    KC_N,    KC_I,    KC_O,    NAV_T(KC_H)
+#define _________________NORMAN_R2_________________ KC_Y,    KC_N,    KC_I,    KC_O,    KC_H
 #define _________________NORMAN_R3_________________ KC_P,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
 
 // QWERTY layer
@@ -71,7 +86,7 @@ enum custom_keycodes {
 #define _________________QWERTY_L3_________________ KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
 
 #define _________________QWERTY_R1_________________ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
-#define _________________QWERTY_R2_________________ KC_H,    KC_J,    KC_K,    KC_L,    NAV_T(KC_SCLN)
+#define _________________QWERTY_R2_________________ KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN
 #define _________________QWERTY_R3_________________ KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
 
 #define LETS_SPLIT(...) LAYOUT_ortho_4x12(__VA_ARGS__)  // macro wrapper to manage with above definitions
@@ -82,13 +97,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            ┌─────┬─────┬─────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬─────┬─────┬─────┐
  *            │ DEL │     │     │     │     │     │   │     │     │     │     │     │     │
  *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
- *            │⌃/TAB│     │     │     │     │     │   │     │     │     │     │ NAV │     │ -- Only when held
+ *            │⌃/TAB│     │     │     │     │     │   │     │     │     │     │     │     │
  *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
  *            │⇧/ESC│     │     │     │     │     │   │     │     │     │     │     │⇧/ENT│
  *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
- *            │     │HYPER│  ⌥  │  ⌘  │LOWER│ SPC │   │BSPC │RAISE│  ⌘  │  ⌥  │HYPER│     │
+ *            │TAP L│HYPER│  ⌥  │  ⌘  │LOWER│ SPC │   │BSPC │RAISE│  ⌘  │  ⌥  │HYPER│TAP R│
  *            └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
- *   NOTE: I had to switch LCMD and LALT layout on base layer to get CMD / ALT to work nicely
  */
 
 /* Base layer (Norman)
@@ -99,14 +113,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
  *            │     │  Z  │  X  │  C  │  V  │  B  │   │  P  │  M  │  ,  │  .  │  /  │     │
  *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
- *            │  `  │     │     │     │     │     │   │     │     │     │     │     │     │
+ *            │     │     │     │     │     │     │   │     │     │     │     │     │     │
  *            └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
  */
 [_NORMAN] = LETS_SPLIT( \
    KC_DEL, _________________NORMAN_L1_________________, _________________NORMAN_R1_________________, KC_BSLS, \
   CTL_TAB, _________________NORMAN_L2_________________, _________________NORMAN_R2_________________, KC_QUOT, \
   SFT_ESC, _________________NORMAN_L3_________________, _________________NORMAN_R3_________________, SFT_ENT, \
-   KC_GRV, KC_HYPR, KC_LALT, KC_LCMD,   LOWER,  KC_SPC, KC_BSPC,   RAISE, KC_RCMD, KC_RALT, KC_HYPR, ___x___  \
+   TAP_L , HYPER_L, KC_LALT, KC_LCMD,   LOWER,  KC_SPC, KC_BSPC,   RAISE, KC_RCMD, KC_RALT, HYPER_R,  TAP_R   \
 ),
 
 /* Base layer (Qwerty, back-up layer)
@@ -117,14 +131,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
  *            │     │  Z  │  X  │  C  │  V  │  B  │   │  N  │  M  │  ,  │  .  │  /  │     │
  *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
- *            │  `  │     │     │     │     │     │   │     │     │     │     │     │     │
+ *            │     │     │     │     │     │     │   │     │     │     │     │     │     │
  *            └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
  */
 [_QWERTY] = LETS_SPLIT( \
   _______, _________________QWERTY_L1_________________, _________________QWERTY_R1_________________, KC_BSLS, \
   _______, _________________QWERTY_L2_________________, _________________QWERTY_R2_________________, KC_QUOT, \
   _______, _________________QWERTY_L3_________________, _________________QWERTY_R3_________________, _______, \
-   KC_GRV, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
 /* Lower (Numbers and Desktop Navigation)
@@ -135,67 +149,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
    *            │     │Next │Vol- │Vol+ │Play │  [  │   │  ]  │  !  │  @  │  #  │  |  │     │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │SCLCK│     │     │     │     │     │   │     │     │     │     │     │     │
+   *            │SCLCK│     │     │     │     │     │   │     │     │  `  │  ~  │     │     │
    *            └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
    */
-[_LOWER] = LAYOUT_ortho_4x12( \
+[_LOWER] = LETS_SPLIT( \
   _______,  SCSHT ,  SCSLC ,   W_L  ,   W_R  , KC_LCBR, KC_RCBR, S(KC_7), S(KC_8), KC_BSLS, KC_LBRC, KC_RBRC, \
   _______,  DSK_L ,  DSK_U ,  DSK_D ,  DSK_R , KC_LPRN, KC_RPRN, S(KC_4), S(KC_5), S(KC_6), KC_LPRN, KC_RPRN, \
   _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, KC_LBRC, KC_RBRC, S(KC_1), S(KC_2), S(KC_3), KC_PIPE, _______, \
-   SCLCK , _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+   SCLCK , _______, _______, _______, _______, _______, _______, _______,  KC_GRV, KC_TILD, _______, _______  \
 ),
 
-/* Raise (Symbols and Function keys)
+/* Raise (Symbols and navigation keys)
    *            ┌─────┬─────┬─────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬─────┬─────┬─────┐
-   *            │     │ F1  │ F2  │ F3  │ F4  │  {  │   │  }  │  7  │  8  │  9  │  /  │  =  │
+   *            │     │     │     │  ↑  │     │  {  │   │  }  │  7  │  8  │  9  │  /  │  =  │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │ F5  │ F6  │ F7  │ F8  │  (  │   │  )  │  4  │  5  │  6  │  *  │  _  │
+   *            │     │     │  ←  │  ↓  │  →  │  (  │   │  )  │  4  │  5  │  6  │  *  │  _  │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │ F9  │ F10 │ F11 │ F12 │  [  │   │  ]  │  1  │  2  │  3  │  -  │  +  │
+   *            │     │     │     │     │     │  [  │   │  ]  │  1  │  2  │  3  │  -  │  +  │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
    *            │     │     │     │     │     │     │   │     │     │  0  │  .  │  ,  │ SPC │
    *            └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
    */
 [_RAISE] = LETS_SPLIT( \
-  _______,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_LCBR, KC_RCBR,   KC_7 ,   KC_8 ,   KC_9 , KC_PSLS, KC_PEQL, \
-  _______,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , KC_LPRN, KC_RPRN,   KC_4 ,   KC_5 ,   KC_6 , KC_PAST, KC_UNDS, \
+  _______,  KC_F1 ,  KC_F2 ,  KC_UP , ___x___, KC_LCBR, KC_RCBR,   KC_7 ,   KC_8 ,   KC_9 , KC_PSLS, KC_PEQL, \
+  _______,  KC_F5 , KC_LEFT, KC_DOWN, KC_RGHT, KC_LPRN, KC_RPRN,   KC_4 ,   KC_5 ,   KC_6 , KC_PAST, KC_UNDS, \
   _______,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, KC_LBRC, KC_RBRC,   KC_1 ,   KC_2 ,   KC_3 , KC_PMNS, KC_PPLS, \
-  _______, _______, _______, _______, _______, _______, _______, _______,   KC_0 ,  KC_DOT, KC_COMM, KC_SPC  \
+  _______, _______, _______, _______, _______, _______, _______, _______,  TAP_0E,  KC_DOT, KC_COMM, KC_SPC  \
 ),
 
-/* Navigation (Triggered by holding right pinky), could add mouse-movements to left hand
+/* Center (Lower + Raise)  (Function keys and misc)
    *            ┌─────┬─────┬─────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬─────┬─────┬─────┐
-   *            │     │     │     │     │     │     │   │     │     │  ↑  │     │     │     │
+   *            │RESET│ F1  │ F2  │ F3  │ F4  │  {  │   │  }  │     │     │     │     │     │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │     │     │     │     │     │   │     │  ←  │  ↓  │  →  │ NAV │     │
+   *            │     │ F5  │ F6  │ F7  │ F8  │  (  │   │  )  │NORMN│QWRTY│     │     │     │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │     │     │     │     │     │   │     │     │     │     │     │     │
+   *            │     │ F9  │ F10 │ F11 │ F12 │  [  │   │  ]  │     │     │     │     │     │
    *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
    *            │     │     │     │     │     │     │   │     │     │     │     │     │     │
    *            └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
    */
-[_NAV] = LETS_SPLIT( \
-  ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___,  KC_UP , ___x___, ___x___, ___x___, \
-  ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, KC_LEFT, KC_DOWN, KC_RGHT, _______, ___x___, \
-  ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, ___x___, \
-  ___x___, ___x___, ___x___, ___x___, _______, _______, _______, _______, ___x___, ___x___, ___x___, ___x___  \
-),
-
-/* Adjust (Lower + Raise)
-   *            ┌─────┬─────┬─────┬─────┬─────┬─────┐   ┌─────┬─────┬─────┬─────┬─────┬─────┐
-   *            │     │     │RESET│     │     │  {  │   │  }  │     │     │     │     │     │
-   *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │     │     │     │     │  (  │   │  )  │NORMN│QWRTY│     │     │     │
-   *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │     │     │     │     │  [  │   │  ]  │     │     │     │     │     │
-   *            ├─────┼─────┼─────┼─────┼─────┼─────┤   ├─────┼─────┼─────┼─────┼─────┼─────┤
-   *            │     │     │     │     │     │     │   │     │     │     │     │     │     │
-   *            └─────┴─────┴─────┴─────┴─────┴─────┘   └─────┴─────┴─────┴─────┴─────┴─────┘
-   */
-[_ADJUST] = LETS_SPLIT( \
-  ___x___, ___x___,  RESET , ___x___, ___x___, KC_LCBR, KC_RCBR, ___x___, ___x___, ___x___, ___x___, ___x___, \
-  ___x___, ___x___, ___x___, ___x___, ___x___, KC_LPRN, KC_RPRN,  NORMAN,  QWERTY, ___x___, ___x___, ___x___, \
-  ___x___, ___x___, ___x___, ___x___, ___x___, KC_LBRC, KC_RBRC, ___x___, ___x___, ___x___, ___x___, ___x___, \
+[_CENTER] = LETS_SPLIT( \
+   RESET ,  KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_LCBR, KC_RCBR, ___x___, ___x___, ___x___, ___x___, ___x___, \
+  ___x___,  KC_F5 ,  KC_F6 ,  KC_F7 ,  KC_F8 , KC_LPRN, KC_RPRN,  NORMAN,  QWERTY, ___x___, ___x___, ___x___, \
+  ___x___,  KC_F9 ,  KC_F10,  KC_F11,  KC_F12, KC_LBRC, KC_RBRC, ___x___, ___x___, ___x___, ___x___, ___x___, \
   ___x___, ___x___, ___x___, ___x___, _______, _______, _______, _______, ___x___, ___x___, ___x___, ___x___  \
 )
 
@@ -223,28 +219,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _CENTER);
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _CENTER);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _CENTER);
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _CENTER);
       }
       return false;
       break;
-    case ADJUST:
+    case CENTER:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        layer_on(_CENTER);
       } else {
-        layer_off(_ADJUST);
+        layer_off(_CENTER);
       }
       return false;
       break;
